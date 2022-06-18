a3faee5eb3cd3530d186022bd690a1a45469a38d
Gittplayer <657386006@qq.com>
2022-06-19 01:39:10 +0800

    优化代码

--- car.py
+++ car.py
@@ -22,12 +22,12 @@ class Car_info_output:
         for car in self.cars:
             car.car_info()
 
-
-vw371 = Car('速腾', '5', '2', '2', '红色', '202220618')
-vw311 = Car('捷达', '5', '2', '2', '黑色', '202220618')
-vw471 = Car('迈腾', '5', '2', '2', '白色', '202220618')
-car_info_output = Car_info_output()
-car_info_output.add(vw371)
-car_info_output.add(vw311)
-car_info_output.add(vw471)
-car_info_output.info_output()
+if __name__ == '__main__':
+    vw371 = Car('速腾', '5', '2', '2', '红色', '202220618')
+    vw311 = Car('捷达', '5', '2', '2', '黑色', '202220618')
+    vw471 = Car('迈腾', '5', '2', '2', '白色', '202220618')
+    car_info_output = Car_info_output()
+    car_info_output.add(vw371)
+    car_info_output.add(vw311)
+    car_info_output.add(vw471)
+    car_info_output.info_output()

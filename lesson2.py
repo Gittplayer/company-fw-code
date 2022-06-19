40f3a9a0b0f2c475671d1197503417d0d7be5c47
Gittplayer <657386006@qq.com>
2022-06-20 02:24:42 +0800

    lesson2 代码优化

--- lesson2.py
+++ lesson2.py
@@ -10,43 +10,19 @@ class Car:
 
 
 class VW371(Car):
-    def _color(self, s):
-        return s
+    def get_color(self, s):
+        print(s)
 
     def _rice(self):
-        if self._color == "gold" or "white":
-            print(150000)
+        if self.get_color == "gold" or "white":
+            print(f"150000 元")
         else:
-            print(120000)
-
-
-# class Car_info_output:
-#     def __init__(self):
-#         self.cars = []
-#
-#     def add(self, car):
-#         self.cars.append(car)
-#
-#     def info_output(self):
-#         for car in self.cars:
-#             car.car_info()
+            print(f"120000 元")
 
 
 if __name__ == '__main__':
-    car1 = Car('速腾', '5', '2', '2')
-    car2 = Car('捷达', '5', '2', '2')
-    carlist = []
-    carlist.append(car1)
-    carlist.append(car2)
-    for i in carlist:
-        i.car_info()
-    # car1.car_info()
-    # car2.car_info()
-    # car_info_output = Car_info_output()
-    # car_info_output.add(car1)
-    # car_info_output.add(car2)
-    # car_info_output.info_output()
+
     vw371 = VW371('速腾', '5', '2', '2')
     vw371.car_info()
-    vw371._color("white")
+    vw371.get_color("white")
     vw371._rice()
